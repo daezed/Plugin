f858970605f96356794d7f45e84d9c845d1b3568
nathan.vanbrunt@gmail.com <nathan23>
2016-02-02 09:15:40 -0500

    fixed the errors it was a index outof bounds error

--- src/main/java/com/nathan/test/School_plugin.java
+++ src/main/java/com/nathan/test/School_plugin.java
@@ -2,6 +2,7 @@ package com.nathan.test;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -10,48 +11,91 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 /**
  * Created by nathan on 2/1/2016.
+ * This is a plugin for minecraft server for schools to use to add a timed schedual that only lets kids in on a spasific time
  */
+
 public class School_plugin extends JavaPlugin implements Listener {
     public Calendar c = GregorianCalendar.getInstance();
-    int[] times= new int[14];
-    boolean timecheack =false;
+    public List<String> time = new ArrayList<String>();
+    public String i = "0|0";
+    public Player pl;
+
     @Override
     public void onEnable() {
-        times[0] = 5;
-        times[1] = 6;
-        times[2] = 7;
-        times[3] = 8;
-        times[4] = 11;
-        times[5] = 13;
-        // TODO Insert logic to be performed when the plugin is enabled
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
-        // TODO Insert logic to be performed when the plugin is disabled
+
     }
 
-
-    /* This is the player join event in this event i am cheching if the player can join*/
-    @EventHandler
-    public  void onPlayerJoin(PlayerJoinEvent event){
-        start();
-        if (timecheack){
-            Player p = event.getPlayer();
-            p.kickPlayer("it is not time ytou you to goine now");
+    @Override// comand to add time to list
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("Addtime")) { // If the player typed /basic then do the following...
+            time.add(args[0]);
+            //i=args[0];
+            Player p = (Player) sender;
+            p.chat(i);
+            return true;
+        } else if (cmd.getName().equalsIgnoreCase("basic2")) {
+            return true;
         }
+        return false;
     }
 
-    private void start() {
-        for (int j = 0; j < times.length; j+=2) {
-            if ((c.get(Calendar.HOUR) >= times[j]) && (c.get(Calendar.HOUR) <= times[j+1])){
-                timecheack=true;
+    @EventHandler //player join event
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+        pl = p;
+
+        if (chaketime()) {
+            p.kickPlayer("not time yet");
+        } else {
+            p.sendMessage(ChatColor.RED + "Welcome to the server");
+
+            /* int db = gettime(1)[0];
+              int db1 = gettime(1)[1];
+              p.chat(String.valueOf(db));
+ -            p.chat(String.valueOf(db1));
+ +            p.chat(String.valueOf(db1));*/
+        }
+        //}
+    }
+
+    //try to get time
+    public boolean chaketime() {
+        int i = 1;
+        for (int j = 0; j < time.size(); j++) {
+            if ((c.get(Calendar.HOUR) >= gettime(i)[0]) && (c.get(Calendar.HOUR) <= gettime(i)[1])) {
+                return true;
+            }
+            pl.chat(String.valueOf(gettime(i)[1]));
+            pl.chat(String.valueOf(gettime(i)[1]));
+            i++;
+        }
+        return false;
+    }
+
+    public int[] gettime(int k/*String in*/) {
+        int[] timei = new int[2];
+        int h=0;
+        for (int i = k - 1; (i < time.size()) && (i < k); i++) {
+            //System.out.println(in.get(i));
+            for (String retval : time.get(i).split("\\|")) {
+                timei[h] = Integer.parseInt(retval);
+                // pl.setDisplayName(time.get(i));
+                //pl.chat(time.get(i));
+                h++;
             }
         }
+        return timei;
     }
 }
